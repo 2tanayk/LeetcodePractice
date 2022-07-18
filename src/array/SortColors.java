d2439a29e45fa5cd7cac3076a440dc32d0465fd8
Tanay Kamath <2tanaykamath@gmail.com>
2022-07-18 18:47:36 +0530

    75. Sort Colors

---
+++ src/array/SortColors.java
@@ -0,0 +1,45 @@
+package array;
+
+import java.util.*;
+
+public class SortColors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sortColors(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sortColors(int[] arr) {
+        int n = arr.length;
+
+        int low = 0, mid = low, high = n - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+
+    }
+
+    private static void swap(int[] arr, int pos1, int pos2) {
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+}
