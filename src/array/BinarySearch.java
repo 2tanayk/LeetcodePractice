7a8e06bd713dbc79f0fb00d5986736f4d8a5dbfd
Tanay Kamath <2tanaykamath@gmail.com>
2022-10-25 22:02:25 +0530

    704. Binary Search

---
+++ src/array/BinarySearch.java
@@ -0,0 +1,41 @@
+package array;
+
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        System.out.println(search(arr, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
